a7f9423ae76a8870d6fc1c9ac8084be474a70300
Gaurav Raj Sharma <blazegaurav123@gmail.com>
2019-08-20 18:07:21 +0545

    - Minor Changes

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,3 +2,5 @@ source "$HOME/.config/fish/alias.fish"
 source "$HOME/.config/fish/shortcuts.fish"
 source	"$HOME/.config/fish/settings.fish"
 set -g simple_ass_prompt_greeting
+
+command -v vg >/dev/null 2>&1; and vg eval --shell fish | source
