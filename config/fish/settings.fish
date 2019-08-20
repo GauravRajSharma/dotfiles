feb43a74c9e67cc5c5ec201b1278071bc70b7920
Gaurav Raj Sharma <blazegaurav123@gmail.com>
2019-08-20 18:07:02 +0545

    - Added Go and Flutter to Path

--- config/fish/settings.fish
+++ config/fish/settings.fish
@@ -1,3 +1,13 @@
 export EDITOR="nvim"
+export ANDROID_HOME=/Users/$USER/Library/Android/sdk
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+set WORK /Volumes/xen/Work
+
 set PATH $PATH ~/Documents/worker/dotfiles/scripts/*
 set PATH $PATH /usr/local/sbin
+set PATH $PATH $ANDROID_HOME/tools
+set PATH $PATH $ANDROID_HOME/platform-tools
+set PATH $PATH $HOME/Documents/Flutter/flutter/bin
+set PATH $PATH $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin
+set PATH $PATH $HOME/go/bin
