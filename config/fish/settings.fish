6eb9a541b2d5140fda9fcc6b36a0e81fb56d25df
Gaurav Raj Sharma <blazegaurav123@gmail.com>
2019-08-21 23:38:19 +0545

    - Added Go path to env for go development

--- config/fish/settings.fish
+++ config/fish/settings.fish
@@ -11,3 +11,6 @@ set PATH $PATH $ANDROID_HOME/platform-tools
 set PATH $PATH $HOME/Documents/Flutter/flutter/bin
 set PATH $PATH $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin
 set PATH $PATH $HOME/go/bin
+
+export GOPATH=$HOME/Documents/programming/Go
+set PATH $PATH $HOME/Documents/programming/Go/bin
