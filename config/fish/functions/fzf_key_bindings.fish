18a0efa5315b984cf413677c020c353a4e3e1ac5
Gaurav Raj Sharma <blazegaurav123@gmail.com>
2019-12-20 19:42:50 +0545

    - 2019/12/20

---
+++ config/fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/Users/gaurav/.fzf/shell/key-bindings.fish
--- config/fish/settings.fish
+++ config/fish/settings.fish
@@ -2,6 +2,7 @@ export EDITOR="nvim"
 export ANDROID_HOME=/Users/$USER/Library/Android/sdk
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+export HOMEBREW_NO_AUTO_UPDATE=1
 set WORK /Volumes/xen/Work
 
 set PATH $PATH ~/Documents/worker/dotfiles/scripts/*
@@ -11,7 +12,9 @@ set PATH $PATH $ANDROID_HOME/platform-tools
 set PATH $PATH $HOME/Documents/Flutter/flutter/bin
 set PATH $PATH $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin
 set PATH $PATH $HOME/go/bin
+set PATH $PATH $HOME/.cargo/bin
 
 export GOPATH=$HOME/Documents/programming/Go
 export GOBIN=$HOME/Documents/programming/Go/bin
 set PATH $PATH $HOME/Documents/programming/Go/bin
+set PATH $PATH /Users/gaurav/Downloads/dkron_2.0.0-rc7_darwin_amd64
